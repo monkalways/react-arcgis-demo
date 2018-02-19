5bbb20986f7281a9cb2d3e0bbe80d831938eb4e0
Wei Wei <weiway@outlook.com>
2018-02-19 11:33:47 -0700

    Add clear filter button

--- src/components/CameraFilter.js
+++ src/components/CameraFilter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Button, Form } from 'semantic-ui-react';
-import { Field, reduxForm } from 'redux-form';
+import { Field, reduxForm, initialize } from 'redux-form';
 
 import { filterCameraLayer } from '../actions/mapActions';
 import renderInput from './utils/renderInput';
@@ -9,16 +9,26 @@ import renderInput from './utils/renderInput';
 const CameraFilter = ({initialValues, pristine, reset, submitting, handleSubmit, filterCameraLayer}) => {
 
     const onSubmit = (values) => {
-        console.log(values);
         filterCameraLayer({
             cameraNumber: values.cameraNumber
         });
+    };
+
+    const handleClearFilters = () => {
+        filterCameraLayer({
+            cameraNumber: null
+        });
+    };
+
+    const isClearFilterButtonDisabled = () => {
+        return !(initialValues && initialValues.cameraNumber);
     };
 
     return (
         <Form onSubmit={handleSubmit(onSubmit)}>
             <Field name='cameraNumber' component={renderInput} label="Camera Number" placeholder='Camera8001' />
             <Button type='submit' disabled={pristine  || submitting} primary>Submit</Button>
+            <Button disabled={isClearFilterButtonDisabled() || submitting} onClick={handleClearFilters}>Clear Filters</Button>
         </Form>
     );
 }
@@ -41,7 +51,6 @@ const CameraFilterReduxForm = reduxForm({
     // validate,
     form: 'camera-filter',
     enableReinitialize: true,
-    // keepDirtyOnReinitialize:true,// 这个值表示重新初始化表单后，不替换已更改的值，可以用clear来测试
 })(CameraFilter);
 
 export default connect(mapStateToProps, {filterCameraLayer})(CameraFilterReduxForm);
