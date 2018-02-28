0e3865fedd1cb78a4a29f8426c93ce246172e9c0
Wei Wei <monkalways@gmail.com>
2018-02-28 15:35:14 -0700

    Remove unused imports from MapFilter

--- src/components/MapFilter.js
+++ src/components/MapFilter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Button, Icon, Segment } from 'semantic-ui-react';
+import { Button, Icon } from 'semantic-ui-react';
 
 import { showFilterModal } from '../actions/appActions';
 import FilterList from './FilterList';
